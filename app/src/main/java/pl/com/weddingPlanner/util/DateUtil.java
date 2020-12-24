078c5a90e0f1ac8fd309a2f5e15e8363dcd927fa
kc3 <kamil.cala@softnet.com.pl>
2020-12-24 02:09:51 +0100

    Added getting tasks by month

--- app/src/main/java/pl/com/weddingPlanner/util/DateUtil.java
+++ app/src/main/java/pl/com/weddingPlanner/util/DateUtil.java
@@ -1,9 +1,17 @@
 package pl.com.weddingPlanner.util;
 
+import android.content.Context;
+
+import org.threeten.bp.LocalDate;
+import org.threeten.bp.Month;
+import org.threeten.bp.format.DateTimeFormatter;
+
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import pl.com.weddingPlanner.R;
 
 public class DateUtil {
 
@@ -22,5 +30,39 @@ public class DateUtil {
         }
 
         return new Date();
+    }
+
+    public static String getDateMonth(String date, Context context) {
+        LocalDate localDate = LocalDate.parse(date, DateTimeFormatter.ISO_DATE);
+        Month month = localDate.getMonth();
+
+        switch (month) {
+            case JANUARY:
+                return context.getResources().getString(R.string.month_january);
+            case FEBRUARY:
+                return context.getResources().getString(R.string.month_february);
+            case MARCH:
+                return context.getResources().getString(R.string.month_march);
+            case APRIL:
+                return context.getResources().getString(R.string.month_april);
+            case MAY:
+                return context.getResources().getString(R.string.month_may);
+            case JUNE:
+                return context.getResources().getString(R.string.month_june);
+            case JULY:
+                return context.getResources().getString(R.string.month_july);
+            case AUGUST:
+                return context.getResources().getString(R.string.month_august);
+            case SEPTEMBER:
+                return context.getResources().getString(R.string.month_september);
+            case OCTOBER:
+                return context.getResources().getString(R.string.month_october);
+            case NOVEMBER:
+                return context.getResources().getString(R.string.month_november);
+            case DECEMBER:
+                return context.getResources().getString(R.string.month_december);
+            default:
+                return month.toString();
+        }
     }
 }
--- app/src/main/java/pl/com/weddingPlanner/view/tasks/TasksMonthFragment.java
+++ app/src/main/java/pl/com/weddingPlanner/view/tasks/TasksMonthFragment.java
@@ -1,5 +1,6 @@
 package pl.com.weddingPlanner.view.tasks;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,15 +10,40 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 import pl.com.weddingPlanner.R;
 import pl.com.weddingPlanner.databinding.FragmentTasksMonthBinding;
+import pl.com.weddingPlanner.model.TaskInfo;
+import pl.com.weddingPlanner.persistence.entity.Task;
+import pl.com.weddingPlanner.util.DAOUtil;
+import pl.com.weddingPlanner.util.DateUtil;
+import pl.com.weddingPlanner.view.list.ContentItem;
+import pl.com.weddingPlanner.view.list.HeaderItem;
+import pl.com.weddingPlanner.view.list.ListItem;
+import pl.com.weddingPlanner.view.list.ListRecyclerAdapter;
+import pl.com.weddingPlanner.view.list.PaginationListenerRecyclerView;
+
+import static pl.com.weddingPlanner.view.list.HeaderItem.getHeaderItemWithDayOfWeek;
+import static pl.com.weddingPlanner.view.list.PaginationListenerRecyclerView.PAGE_START;
 
 public class TasksMonthFragment extends Fragment {
 
     private FragmentTasksMonthBinding binding;
 
     private final String month;
+
+    private int currentPage = PAGE_START;
+    private boolean isLastPage = false;
+    private boolean isLoading = false;
+    private LinearLayoutManager linearLayoutManager;
+    private ListRecyclerAdapter adapter;
 
     public TasksMonthFragment(String month) {
         this.month = month;
@@ -29,12 +55,92 @@ public class TasksMonthFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_tasks_month, container, false);
 
-        setMonthText();
+        setRecyclerView();
+        setSwipeRefresh();
+
+        getTasksList();
 
         return binding.getRoot();
     }
 
-    private void setMonthText() {
-        binding.month.setText(month);
+    private void setRecyclerView() {
+        linearLayoutManager = new LinearLayoutManager(requireContext());
+        adapter = new ListRecyclerAdapter(requireContext(), new LinkedList<>(), item -> {
+            Intent intent = new Intent(requireContext(), TaskDetailsActivity.class);
+//            intent.putExtra(ACCOUNT_NUMBER, item.getDetailsId()); //TODO put id extra
+            startActivity(intent);
+        });
+
+        RecyclerView recyclerView = binding.recyclerView;
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(adapter);
+
+        recyclerView.addOnScrollListener(new PaginationListenerRecyclerView(linearLayoutManager) {
+            @Override
+            public boolean isLoading() {
+                return isLoading;
+            }
+
+            @Override
+            public boolean isLastPage() {
+                return isLastPage;
+            }
+
+            @Override
+            protected void loadMoreItems() {
+                isLoading = true;
+                currentPage++;
+//                getAccounts(currentPage);
+            }
+        });
+    }
+
+    private void setSwipeRefresh() {
+        SwipeRefreshLayout swipeRefresh = binding.swipeRefresh;
+        swipeRefresh.setOnRefreshListener(() -> {
+            swipeRefresh.setRefreshing(false);
+            adapter.clear();
+            currentPage = PAGE_START;
+            getTasksList();
+        });
+    }
+
+    private void getTasksList() {
+        List<TaskInfo> toReturn = new ArrayList<>();
+        List<Task> allTasks = DAOUtil.getAllTasks(requireContext());
+
+        List<Task> tasksByMonth = new ArrayList<>();
+        for (Task task : allTasks) {
+            if (month.equals(DateUtil.getDateMonth(task.getDate(), requireContext()))) {
+                tasksByMonth.add(task);
+            }
+        }
+
+        for (Task task : tasksByMonth) {
+            TaskInfo taskInfo = TaskInfo.builder()
+                    .itemId(task.getId())
+                    .title(task.getTitle())
+                    .date(task.getDate())
+                    .build();
+            toReturn.add(taskInfo);
+        }
+
+        List<ListItem> listItems = prepareAccountsInfoList(toReturn, adapter.getItems());
+        adapter.addItems(listItems);
+    }
+
+    private List<ListItem> prepareAccountsInfoList(List<TaskInfo> taskInfoList, List<ListItem> list) {
+        List<ListItem> toReturn = new ArrayList<>();
+
+        for (TaskInfo taskInfo : taskInfoList) {
+            HeaderItem headerItem = getHeaderItemWithDayOfWeek(requireContext(), taskInfo.getDate());
+
+            if (!toReturn.contains(headerItem) && !list.contains(headerItem)) {
+                toReturn.add(headerItem);
+            }
+            toReturn.add(ContentItem.of(taskInfo));
+        }
+
+        return toReturn;
     }
 }
