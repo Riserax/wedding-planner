8c69b31df1cc5df15fb9c663648904a5ef71bf4d
kc3 <kamil.cala@softnet.com.pl>
2021-05-05 00:49:45 +0200

    Add forms for sign-up and sign-in

--- app/src/main/java/pl/com/weddingPlanner/view/authentication/CreateUserActivity.java
+++ app/src/main/java/pl/com/weddingPlanner/view/authentication/CreateUserActivity.java
@@ -1,7 +1,11 @@
 package pl.com.weddingPlanner.view.authentication;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.databinding.DataBindingUtil;
@@ -15,6 +19,10 @@ import pl.com.weddingPlanner.R;
 import pl.com.weddingPlanner.databinding.ActivityCreateUserBinding;
 import pl.com.weddingPlanner.view.BaseActivity;
 import pl.com.weddingPlanner.view.NavigationActivity;
+import pl.com.weddingPlanner.view.util.ComponentsUtil;
+
+import static pl.com.weddingPlanner.view.util.ComponentsUtil.setButtonEnability;
+import static pl.com.weddingPlanner.view.util.LambdaUtil.getOnTextChangedTextWatcher;
 
 public class CreateUserActivity extends BaseActivity {
 
@@ -33,15 +41,72 @@ public class CreateUserActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_create_user);
 
+        setButtonEnability(binding.registerButton, false);
         setListeners();
     }
 
     private void setListeners() {
-        binding.registerButton.setOnClickListener(v -> handleCreateUserWithEmailAndPassword("camil.kala@gmail.com", "haslo123"));
-        binding.signInButton.setOnClickListener(v -> startActivity(new Intent(CreateUserActivity.this, SignInActivity.class)));
+        setRegisterButtonEnableStatusListener();
+        setRootLayoutListener();
+        setOnFocusChangeListener();
+
+        binding.registerButton.setOnClickListener(v -> {
+            handleCreateUserWithEmailAndPassword(binding.email.getText().toString(), binding.password.getText().toString());
+        });
+
+        binding.signInButton.setOnClickListener(v -> {
+            startActivity(new Intent(CreateUserActivity.this, SignInActivity.class));
+        });
+
+        binding.regulationCheckbox.setOnCheckedChangeListener((compoundButton, b) -> {
+            setButtonEnability(binding.registerButton, areFieldsValid());
+            ComponentsUtil.hideKeyboard(this, getCurrentFocus());
+        });
+    }
+
+    private void setRegisterButtonEnableStatusListener() {
+        TextWatcher listener = getOnTextChangedTextWatcher((s, start, before, count) ->
+                setButtonEnability(binding.registerButton, areFieldsValid())
+        );
+
+        binding.email.addTextChangedListener(listener);
+        binding.password.addTextChangedListener(listener);
+    }
+
+    private boolean areFieldsValid() {
+        return !binding.email.getText().toString().isEmpty()
+                && !binding.password.getText().toString().isEmpty()
+                && binding.regulationCheckbox.isChecked();
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void setRootLayoutListener() {
+        binding.rootLayout.setOnTouchListener((v, event) -> {
+            clearFocusAndHideKeyboard();
+            return false;
+        });
+    }
+
+    private void clearFocusAndHideKeyboard() {
+        View focused = getCurrentFocus();
+        if (focused instanceof EditText) {
+            focused.clearFocus();
+            ComponentsUtil.hideKeyboard(this, focused);
+        }
+    }
+
+    private void setOnFocusChangeListener() {
+        final View.OnFocusChangeListener listener = (v, hasFocus) -> {
+            if (!hasFocus) {
+                ComponentsUtil.hideKeyboard(this, v);
+            }
+        };
+
+        binding.registerButton.setOnFocusChangeListener(listener);
     }
 
     private void handleCreateUserWithEmailAndPassword(String email, String password) {
+        //TODO walidacja email + hasło
         firebaseAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, this::handleRegistrationTask);
     }
--- app/src/main/java/pl/com/weddingPlanner/view/authentication/SignInActivity.java
+++ app/src/main/java/pl/com/weddingPlanner/view/authentication/SignInActivity.java
@@ -1,7 +1,11 @@
 package pl.com.weddingPlanner.view.authentication;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.databinding.DataBindingUtil;
@@ -14,6 +18,10 @@ import pl.com.weddingPlanner.R;
 import pl.com.weddingPlanner.databinding.ActivitySignInBinding;
 import pl.com.weddingPlanner.view.BaseActivity;
 import pl.com.weddingPlanner.view.NavigationActivity;
+import pl.com.weddingPlanner.view.util.ComponentsUtil;
+
+import static pl.com.weddingPlanner.view.util.ComponentsUtil.setButtonEnability;
+import static pl.com.weddingPlanner.view.util.LambdaUtil.getOnTextChangedTextWatcher;
 
 public class SignInActivity extends BaseActivity {
 
@@ -32,12 +40,62 @@ public class SignInActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_in);
 
+        setButtonEnability(binding.signInButton, false);
         setListeners();
     }
 
     private void setListeners() {
-        binding.signInButton.setOnClickListener(v -> handleSignInWithEmailAndPassword("camil.kala@gmail.com", "haslo123"));
-        binding.registerButton.setOnClickListener(v -> startActivity(new Intent(SignInActivity.this, CreateUserActivity.class)));
+        setSignInButtonEnableStatusListener();
+        setRootLayoutListener();
+        setOnFocusChangeListener();
+
+        binding.signInButton.setOnClickListener(v -> {
+            handleSignInWithEmailAndPassword(binding.email.getText().toString(), binding.password.getText().toString());
+        });
+
+        binding.registerButton.setOnClickListener(v -> {
+            startActivity(new Intent(SignInActivity.this, CreateUserActivity.class));
+        });
+    }
+
+    private void setSignInButtonEnableStatusListener() {
+        TextWatcher listener = getOnTextChangedTextWatcher((s, start, before, count) ->
+                setButtonEnability(binding.signInButton, areFieldsValid())
+        );
+
+        binding.email.addTextChangedListener(listener);
+        binding.password.addTextChangedListener(listener);
+    }
+
+    private boolean areFieldsValid() {
+        return !binding.email.getText().toString().isEmpty()
+                && !binding.password.getText().toString().isEmpty();
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void setRootLayoutListener() {
+        binding.rootLayout.setOnTouchListener((v, event) -> {
+            clearFocusAndHideKeyboard();
+            return false;
+        });
+    }
+
+    private void clearFocusAndHideKeyboard() {
+        View focused = getCurrentFocus();
+        if (focused instanceof EditText) {
+            focused.clearFocus();
+            ComponentsUtil.hideKeyboard(this, focused);
+        }
+    }
+
+    private void setOnFocusChangeListener() {
+        final View.OnFocusChangeListener listener = (v, hasFocus) -> {
+            if (!hasFocus) {
+                ComponentsUtil.hideKeyboard(this, v);
+            }
+        };
+
+        binding.signInButton.setOnFocusChangeListener(listener);
     }
 
     private void handleSignInWithEmailAndPassword(String email, String password) {
